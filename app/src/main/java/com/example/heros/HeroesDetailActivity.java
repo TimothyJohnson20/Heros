beb65137c9716c79b915e79793b283761cb693d7
Timothy Johnson <timothyjohnsontimothy@gmail.com>
2020-03-13 14:08:14 -0700

    FINISHED PROJECT

--- app/src/main/java/com/example/heros/HeroesDetailActivity.java
+++ app/src/main/java/com/example/heros/HeroesDetailActivity.java
@@ -40,20 +40,6 @@ public class HeroesDetailActivity extends AppCompatActivity {
 
     }
 
-//    public String readTextFile(InputStream inputStream) {
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//        byte buf[] = new byte[1024];
-//        int len;
-//        try {
-//            while ((len = inputStream.read(buf)) != -1) {
-//                outputStream.write(buf, 0, len);
-//            }
-//            outputStream.close();
-//            inputStream.close();
-//        } catch (IOException e) {
-//        }
-//        return outputStream.toString();
-//    }
 
     public void wireWidgets(){
         imageView_heroImage = findViewById(R.id.imageView_heroes_image);
